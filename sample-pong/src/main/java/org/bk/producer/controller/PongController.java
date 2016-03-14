b147e3869f6bb1a0e813ec8810004bc41a15f0f3
Sean Chao <schao@gatech.edu>
2016-03-13 21:46:34 -0400

    3rd

---
+++ sample-pong/src/main/java/org/bk/producer/controller/PongController.java
@@ -0,0 +1,32 @@
+package org.bk.producer.controller;
+
+
+import org.bk.producer.domain.MessageAcknowledgement;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PongController {
+
+    @Value("${reply.message}")
+    private String message;
+
+//    @RequestMapping(value = "/message", method = RequestMethod.POST)
+//    public Resource<MessageAcknowledgement> pongMessage(@RequestBody Message input) {
+//    return new Resource<>(
+//                new MessageAcknowledgement(input.getId(), input.getPayload(), message));
+//    }
+
+    @RequestMapping(value = "/message", method = RequestMethod.POST)
+  public Resource<MessageAcknowledgement> pongMessage(@RequestBody String input) {
+    	System.out.println("Pong received it");
+    	return new Resource<>(
+              new MessageAcknowledgement("1", "ROGER THAT", "${reply.message}"));
+  }
+
+
+}
